ca133e7ec9a82a8ff914bfab5d3b20c08398de46
Francisco Di Paolo <franciscogermandipaolo@gmail.com>
2021-10-19 11:31:53 -0300

    Primera Persona
    Mecanica Primera Persona

---
+++ Primera Persona/Assets/Script/ControlGame.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGame : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Liana;
+    private List<GameObject> listaEnemigos;
+    float tiempoRestante;
+    void Start()
+    {
+        ComenzarJuego();
+    }
+
+    void Update()
+    {
+        if (tiempoRestante == 0)
+        {
+            ComenzarJuego();
+        }
+    }
+
+    void ComenzarJuego()
+    {
+        Player.transform.position = new Vector3(0f, 0f, 0f);
+
+        foreach (GameObject item in listaEnemigos)
+        {
+            Destroy(item);
+        }
+
+        listaEnemigos.Add(Instantiate(Liana, new Vector3(5, 1f, 3f), Quaternion.identity));
+        listaEnemigos.Add(Instantiate(Liana, new Vector3(3, 1f, 3f), Quaternion.identity));
+        listaEnemigos.Add(Instantiate(Liana, new Vector3(1, 1f, 3f), Quaternion.identity));
+        StartCoroutine(ComenzarCronometro(30));
+    }
+
+    public IEnumerator ComenzarCronometro(float valorCronometro = 30)
+    {
+        tiempoRestante = valorCronometro;
+        while (tiempoRestante > 0)
+        {
+            Debug.Log("Restan " + tiempoRestante + " segundos.");
+            yield return new WaitForSeconds(1.0f);
+            tiempoRestante--;
+        }
+    }
+}
---
+++ Primera Persona/Assets/Script/ControlLiana.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLiana : MonoBehaviour
+{
+    private int hp;
+    void Start()
+    {
+        hp = 100;
+    }
+
+    public void recibirDaño()
+    {
+        hp = hp - 100;
+        if (hp <= 0)
+        {
+            this.desaparecer();
+        }
+    }
+
+    public void desaparecer()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bala"))
+        {
+            recibirDaño();
+        }
+    }
+}
